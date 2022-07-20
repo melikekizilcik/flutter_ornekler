f4cafc3f8b44c34d8701c95e6795f9293d8422fb
Melike <melikeekizilcikk@gmail.com>
2022-07-20 14:18:45 +0300

    navigation ile ilgili ornekler eklendi

--- navigation_learn/navigation_learn/lib/main.dart
+++ navigation_learn/navigation_learn/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:navigation_learn/pink_page.dart';
 import 'package:navigation_learn/red_page.dart';
 
 void main() {
@@ -16,7 +17,21 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Anasayfa(),
+      //home: Anasayfa(),
+      routes: {
+        'redPage': (context) => RedPage(),
+        '/': (context) => Anasayfa(),
+        '/pinkPage': (context) => PinkPage(),
+      },
+      onUnknownRoute: (setting) => MaterialPageRoute(
+          builder: (context) => Scaffold(
+                appBar: AppBar(
+                  title: Text("Error"),
+                ),
+                body: Center(
+                  child: Text("404"),
+                ),
+              )),
     );
   }
 }
@@ -50,6 +65,53 @@ class Anasayfa extends StatelessWidget {
             child: Text("Kırmızı sayfaya git Android"),
             style: ElevatedButton.styleFrom(primary: Colors.red.shade600),
           ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).maybePop();
+              },
+              child: Text(
+                "Maybe Pop Kullanımı",
+                //Eğer geri gidebilecek başka eleman varsa gider yoksa gitmez
+              )),
+          ElevatedButton(
+              onPressed: () {
+                if (Navigator.of(context).canPop()) {
+                  print("Pop olabilir");
+                } else {
+                  print("hayır olamaz");
+                }
+              },
+              child: Text(
+                "canPop Kullanımı",
+                //Eğer geri gidebilecek başka eleman varsa gider yoksa gitmez
+              )),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => PinkPage()));
+              },
+              child: Text(
+                "Push Replacement Kullanımı",
+                //Eğer geri gidebilecek başka eleman varsa gider yoksa gitmez
+              )),
+          ElevatedButton(
+              onPressed: () {
+                // Navigator.of(context).pushNamed(routeName);
+                Navigator.pushNamed(context, '/redPage');
+              },
+              child: Text(
+                "PushNamed Kullanımı",
+                //Eğer geri gidebilecek başka eleman varsa gider yoksa gitmez
+              )),
+          ElevatedButton(
+              onPressed: () {
+                // Navigator.of(context).pushNamed(routeName);
+                Navigator.pushNamed(context, '/pinkPage2');
+              },
+              child: Text(
+                "Go to Pink",
+                //Eğer geri gidebilecek başka eleman varsa gider yoksa gitmez
+              ))
         ],
       )),
     );
--- navigation_learn/navigation_learn/lib/red_page.dart
+++ navigation_learn/navigation_learn/lib/red_page.dart
@@ -8,27 +8,34 @@ class RedPage extends StatelessWidget {
   int _rastgeleSayi = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Red Page"),
-        backgroundColor: Colors.red,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              "Red Page",
-              style: TextStyle(fontSize: 24),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _rastgeleSayi = Random().nextInt(100);
-                print("Üretilen sayı $_rastgeleSayi");
-                Navigator.of(context).pop(_rastgeleSayi);
-              },
-              child: Text("Geri Dön"),
-            )
-          ],
+    return WillPopScope(
+      onWillPop: () {
+        print("on will pop çalıştı");
+        _rastgeleSayi = Random().nextInt(100);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Red Page"),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text(
+                "Red Page",
+                style: TextStyle(fontSize: 24),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _rastgeleSayi = Random().nextInt(100);
+                  print("Üretilen sayı $_rastgeleSayi");
+                  Navigator.of(context).pop(_rastgeleSayi);
+                },
+                child: Text("Geri Dön"),
+              )
+            ],
+          ),
         ),
       ),
     );
